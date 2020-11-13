15ad48901fd4e03fe85ab38377be399a63839c84
Bruno de Marzio <brunodemarzio@hotmail.com>
2020-11-13 20:17:16 -0300

    Métodos do relatório adicionados (precisa terminar)

--- PI3/src/main/java/PacotePrincipal/RelatorioDAO.java
+++ PI3/src/main/java/PacotePrincipal/RelatorioDAO.java
@@ -48,7 +48,7 @@ public class RelatorioDAO {
         return listaVendas;
     }
 
-    public static List<Relatorio> getVendas(String cpfCli) {
+    public static List<Relatorio> getVendasCPF(String cpfCli) {
         List<Relatorio> listaVendas = new ArrayList();
         try {
             Connection con = ConexaoBD.getConexao();
@@ -80,5 +80,70 @@ public class RelatorioDAO {
         }
         return listaVendas;
     }
+    
+    public static List<Relatorio> getVendasCat(String categoria){
+        List<Relatorio> listaVendas = new ArrayList();
+        try {
+            Connection con = ConexaoBD.getConexao();
+            String query = "select v.IDVENDA, v.ID, p.NOME, v.QUANTIDADE, cli.NOME as cli, cli.CPF, p.PRECO, f.IDFILIAL, f.NOME as filial, c.TITULO, c.ID from venda on v.categoria = " + categoria + "\n"
+                    + " join produto p on v.ID = p.ID\n"
+                    + " join categoria c on p.IDCATEGORIA = c.ID\n"
+                    + " join cliente cli on v.CPF = cli.CPF\n"
+                    + " join filial f on v.IDFILIAL = f.IDFILIAL";
+            PreparedStatement ps = con.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            String categoriaSTR = categoria;
+            while (rs.next()) {
+                int idVenda = rs.getInt("IDVENDA");
+                int idProduto = rs.getInt("ID");
+                String nomeProduto = rs.getString("NOME");
+                int quantidade = rs.getInt("QUANTIDADE");
+                String nomeCliente = rs.getString("cli");
+                long cpf = rs.getLong("CPF");
+                double preco = rs.getDouble("PRECO");
+                int filial = rs.getInt("IDFILIAL");
+                String nomeFilial = rs.getString("filial");
 
+                int idCategoria = rs.getInt("ID");
+//                listaVendas.add(new Relatorio());
+                listaVendas.add(new Relatorio(idVenda, idProduto, nomeProduto, cpf, nomeCliente, quantidade, preco, filial, nomeFilial, idCategoria, categoriaSTR));
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(ServletBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listaVendas;
+    }
+
+    public static List<Relatorio> getVendasFili(String filial){
+        List<Relatorio> listaVendas = new ArrayList();
+        try {
+            Connection con = ConexaoBD.getConexao();
+            String query = "select v.IDVENDA, v.ID, p.NOME, v.QUANTIDADE, cli.NOME as cli, cli.CPF, p.PRECO, f.IDFILIAL, f.NOME as filial, c.TITULO, c.ID from venda on v.filial = " + filial + "\n"
+                    + " join produto p on v.ID = p.ID\n"
+                    + " join categoria c on p.IDCATEGORIA = c.ID\n"
+                    + " join cliente cli on v.CPF = cli.CPF\n"
+                    + " join filial f on v.IDFILIAL = f.IDFILIAL";
+            PreparedStatement ps = con.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            int filialInt = Integer.parseInt(filial);
+            while (rs.next()) {
+                int idVenda = rs.getInt("IDVENDA");
+                int idProduto = rs.getInt("ID");
+                String nomeProduto = rs.getString("NOME");
+                int quantidade = rs.getInt("QUANTIDADE");
+                String nomeCliente = rs.getString("cli");
+                long cpf = rs.getLong("CPF");
+                double preco = rs.getDouble("PRECO");
+
+                String nomeFilial = rs.getString("filial");
+                String categoria = rs.getString("TITULO");
+                int idCategoria = rs.getInt("ID");
+//                listaVendas.add(new Relatorio());
+                listaVendas.add(new Relatorio(idVenda, idProduto, nomeProduto, cpf, nomeCliente, quantidade, preco, filialInt, nomeFilial, idCategoria, categoria));
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(ServletBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listaVendas;
+    }
 }
